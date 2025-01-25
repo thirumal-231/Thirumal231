867f1cdb022712eb6ef374c6b5b688a2a6134712
Thirumala Reddy <o170231thiru@gmail.com>
2025-01-25 21:10:51 +0530

    Updated projects

--- js/script.js
+++ js/script.js
@@ -101,11 +101,11 @@ const activePortfolio = () => {
 };
 
 arrowRight.addEventListener('click', () => {
-  if (index < 4) {
+  if (index < 1) {
     index++;
     arrowLeft.classList.remove('disabled');
   } else {
-    index = 5;
+    index = 2;
     arrowRight.classList.add('disabled');
   }
   activePortfolio();
