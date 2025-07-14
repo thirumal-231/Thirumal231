592b563f433a939f9ce7d224d892563cde5e486d
Thirumala Reddy <o170231thiru@gmail.com>
2025-07-15 01:20:03 +0530

    Add Natours API project

--- js/script.js
+++ js/script.js
@@ -1,123 +1,124 @@
-const navLinks = document.querySelectorAll('header nav a');
-const logoLink = document.querySelector('.logo');
-const sections = document.querySelectorAll('section');
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('header nav');
+const navLinks = document.querySelectorAll("header nav a");
+const logoLink = document.querySelector(".logo");
+const sections = document.querySelectorAll("section");
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector("header nav");
 
-menuIcon.addEventListener('click', () => {
-  menuIcon.classList.toggle('bx-x');
-  navbar.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 });
 
 const activePage = () => {
-  const header = document.querySelector('header');
-  const barsBox = document.querySelector('.bars-box');
+  const header = document.querySelector("header");
+  const barsBox = document.querySelector(".bars-box");
 
-  header.classList.remove('active');
+  header.classList.remove("active");
   setTimeout(() => {
-    header.classList.add('active');
+    header.classList.add("active");
   }, 1100);
 
   navLinks.forEach((link) => {
-    link.classList.remove('active');
+    link.classList.remove("active");
   });
 
-  barsBox.classList.remove('active');
+  barsBox.classList.remove("active");
   setTimeout(() => {
-    barsBox.classList.add('active');
+    barsBox.classList.add("active");
   }, 1100);
 
   sections.forEach((section) => {
-    section.classList.remove('active');
+    section.classList.remove("active");
   });
 
-  menuIcon.classList.remove('bx-x');
-  navbar.classList.remove('active');
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
 navLinks.forEach((link, idx) => {
-  link.addEventListener('click', () => {
-    if (!link.classList.contains('active')) {
+  link.addEventListener("click", () => {
+    if (!link.classList.contains("active")) {
       activePage();
 
-      link.classList.add('active');
+      link.classList.add("active");
 
       setTimeout(() => {
-        sections[idx].classList.add('active');
+        sections[idx].classList.add("active");
       }, 1100);
     }
   });
 });
 
-logoLink.addEventListener('click', () => {
-  if (!navLinks[0].classList.contains('active')) {
+logoLink.addEventListener("click", () => {
+  if (!navLinks[0].classList.contains("active")) {
     activePage();
 
-    navLinks[0].classList.add('active');
+    navLinks[0].classList.add("active");
 
     setTimeout(() => {
-      sections[0].classList.add('active');
+      sections[0].classList.add("active");
     }, 1100);
   }
 });
 
-const resumeBtns = document.querySelectorAll('.resume-btn');
+const resumeBtns = document.querySelectorAll(".resume-btn");
 resumeBtns.forEach((btn, idx) => {
-  btn.addEventListener('click', () => {
-    const resumeDetails = document.querySelectorAll('.resume-detail');
+  btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-detail");
     resumeBtns.forEach((btn) => {
-      btn.classList.remove('active');
+      btn.classList.remove("active");
     });
-    btn.classList.add('active');
+    btn.classList.add("active");
 
     resumeDetails.forEach((detail) => {
-      detail.classList.remove('active');
+      detail.classList.remove("active");
     });
-    resumeDetails[idx].classList.add('active');
+    resumeDetails[idx].classList.add("active");
   });
 });
 
 const arrowRight = document.querySelector(
-  '.portfolio-box .navigation .arrow-right'
+  ".portfolio-box .navigation .arrow-right"
 );
 const arrowLeft = document.querySelector(
-  '.portfolio-box .navigation .arrow-left'
+  ".portfolio-box .navigation .arrow-left"
 );
 
 let index = 0;
 
 const activePortfolio = () => {
-  const imgSlide = document.querySelector('.portfolio-carousal .img-slide');
-  const portfolioDetails = document.querySelectorAll('.portfolio-detail');
+  console.log(index);
+  const imgSlide = document.querySelector(".portfolio-carousal .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-detail");
 
   imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
     index * 2
   }rem))`;
 
   portfolioDetails.forEach((detail) => {
-    detail.classList.remove('active');
+    detail.classList.remove("active");
   });
-  portfolioDetails[index].classList.add('active');
+  portfolioDetails[index].classList.add("active");
 };
 
-arrowRight.addEventListener('click', () => {
-  if (index < 1) {
+arrowRight.addEventListener("click", () => {
+  if (index < 2) {
     index++;
-    arrowLeft.classList.remove('disabled');
+    arrowLeft.classList.remove("disabled");
   } else {
-    index = 2;
-    arrowRight.classList.add('disabled');
+    index = 3;
+    arrowRight.classList.add("disabled");
   }
   activePortfolio();
 });
 
-arrowLeft.addEventListener('click', () => {
+arrowLeft.addEventListener("click", () => {
   if (index > 1) {
     index--;
-    arrowRight.classList.remove('disabled');
+    arrowRight.classList.remove("disabled");
   } else {
     index = 0;
-    arrowLeft.classList.add('disabled');
+    arrowLeft.classList.add("disabled");
   }
   activePortfolio();
 });
